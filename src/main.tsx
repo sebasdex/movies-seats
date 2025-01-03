f73c1c65f069af75f38a27e1074990ccc4b0d3c3
DexterDC <sebasdc9@hotmail.com>
2025-01-03 14:49:49 -0600

    config react-router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router";
+import App from "./App";
+import "./index.css";
+import Layout from "./Layout";
+import Seats from "./components/Seats";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root = document.getElementById("root") as HTMLElement;
+ReactDOM.createRoot(root).render(
+  <BrowserRouter>
+    <Routes>
+      <Route element={<Layout />}>
+        <Route path="/" element={<App />} />
+        <Route path="/seats" element={<Seats />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
+);
