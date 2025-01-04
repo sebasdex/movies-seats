b9662efe1c3bba4c7e2eeeb5e8c55960f1517097
DexterDC <sebasdc9@hotmail.com>
2025-01-03 18:45:18 -0600

    add seats components

--- src/components/Seats.tsx
+++ src/components/Seats.tsx
@@ -1,45 +1,50 @@
-import { useNavigate } from "react-router"
+import SeatsCost from "./seatsCost"
+
 function Seats() {
-    const navigate = useNavigate()
+    const rows = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
+    const cols = [11, 11, 13, 13, 17, 17, 17, 13];
+
     return (
-        <section className="flex text-white min-h-screen">
-            <section className="flex flex-col absolute top-32 left-50">
-                <article className="h-[30rem] w-80 bg-black">
-                    <img src="https://moviepostermexico.com/cdn/shop/products/inception_ver2_xxlg_1024x1024@2x.jpg?v=1574870710"
-                        alt="movie" className="h-full w-full object-cover object-top" />
-                </article>
-                <article className="my-8 flex flex-col gap-2 p-2">
-                    <div className="flex items-center justify-between gap-4 p-2 border">
-                        <p className="flex-1">Asiento: A2</p>
-                        <p className="text-gray-400">$80</p>
-                        <button className="">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-x"><path d="M18 6 6 18" /><path d="m6 6 12 12" /></svg>
-                        </button>
+        <section className="flex justify-between text-white relative">
+            <SeatsCost />
+            <section className="flex flex-col items-center mb-20 w-full ml-96 -mt-10">
+                {/* Figura de pantalla del cine */}
+                <section className="relative">
+                    <div className="movie-screen"></div>
+                    <div className="movie-shadow"></div>
+                </section>
+                {/* Fila de asientos */}
+                <section className="w-full">
+                    <div className="w-[45rem] mx-auto flex flex-col items-center justify-center">
+                        {
+                            rows.map((row, rowIndex) => (
+                                <div
+                                    key={rowIndex}
+                                    className="grid gap-1 mb-1"
+                                    style={{
+                                        gridTemplateColumns: `repeat(${cols[rowIndex]}, 2rem)`,
+                                        gridAutoRows: `2rem`
+                                    }}
+                                >
+                                    {
+                                        Array.from({ length: cols[rowIndex] }, (_, i) => (
+                                            <button
+                                                key={`${rowIndex}-${i}`}
+                                                className="border border-white/30 rounded-lg p-1 text-center text-xs hover:bg-red-700 hover:text-white"
+                                            >
+                                                {`${row}${i + 1}`}
+                                            </button>
+                                        ))
+                                    }
+                                </div>
+                            ))
+                        }
                     </div>
-                    <div className="flex items-center justify-between gap-4 p-2 border">
-                        <p className="flex-1">Asiento: A2</p>
-                        <p className="text-gray-400">$80</p>
-                        <button className="">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-x"><path d="M18 6 6 18" /><path d="m6 6 12 12" /></svg>
-                        </button>
-                    </div>
-                    <div className="flex items-center justify-between gap-4 p-2 border">
-                        <p className="flex-1">Asiento: A2</p>
-                        <p className="text-gray-400">$80</p>
-                        <button className="">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-x"><path d="M18 6 6 18" /><path d="m6 6 12 12" /></svg>
-                        </button>
-                    </div>
-                    <div className="flex justify-end">
-                        <p className="p-2 text-white/30">Total:
-                            <span className="text-white ml-2 text-xl">$80</span>
-                        </p>
-                    </div>
-                    <div className="grid grid-cols-2 gap-2 mt-8">
-                        <button className="btn btn-outline btn-error" onClick={() => navigate("/")}>Cancelar</button>
-                        <button className="btn btn-outline btn-error">Reservar</button>
-                    </div>
-                </article>
+                </section>
+
+
+
+
             </section>
         </section>
     )
