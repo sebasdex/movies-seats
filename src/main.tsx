f5eeb4581d00ab963f0944b134b8c9e8294204df
DexterDC <sebasdc9@hotmail.com>
2025-01-05 17:57:34 -0600

    wrap context in app

--- src/main.tsx
+++ src/main.tsx
@@ -4,15 +4,18 @@ import App from "./App";
 import "./index.css";
 import Layout from "./Layout";
 import Seats from "./components/Seats";
+import { TheaterProvider } from "./context/TheaterProvider";
 
 const root = document.getElementById("root") as HTMLElement;
 ReactDOM.createRoot(root).render(
   <BrowserRouter>
-    <Routes>
-      <Route element={<Layout />}>
-        <Route path="/" element={<App />} />
-        <Route path="/seats/:slugId" element={<Seats />} />
-      </Route>
-    </Routes>
+    <TheaterProvider>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<App />} />
+          <Route path="/seats/:slugId" element={<Seats />} />
+        </Route>
+      </Routes>
+    </TheaterProvider>
   </BrowserRouter>
 );
