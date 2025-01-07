f588279b32b344eb4ac74f823a1df914d74b55cc
dexterdc <sebasdc9@hotmail.com>
2025-01-07 09:45:49 -0600

    update array of theaters

--- src/context/FilterContext.tsx
+++ src/context/FilterContext.tsx
@@ -1,15 +1,16 @@
 import { createContext } from "react";
-import { TheaterHourInfo, Theater } from "../types/theater"
+import { TheaterHourInfo, Theater } from "../types/theater";
 
 type TheaterContextType = {
-    theaterInfo: (id: number) => Theater[];
-    idMovieFunction
-    : (idSlug: string) => number;
-    setTheaterHourInfo: React.Dispatch<React.SetStateAction<TheaterHourInfo[]>>;
-    theaterHourInfo: TheaterHourInfo[];
-    isChecked: boolean;
-    setIsChecked: React.Dispatch<React.SetStateAction<boolean>>;
-}
+  theaterInfo: (id: number) => Theater[];
+  idMovieFunction: (idSlug: string) => number;
+  setTheaterHourInfo: React.Dispatch<React.SetStateAction<TheaterHourInfo[]>>;
+  theaterHourInfo: TheaterHourInfo[];
+  isChecked: boolean;
+  setIsChecked: React.Dispatch<React.SetStateAction<boolean>>;
+  theatersArray: Theater[];
+  setTheatersArray: React.Dispatch<React.SetStateAction<Theater[]>>;
+};
 
 const TheaterContext = createContext<TheaterContextType | undefined>(undefined);
 
--- src/context/TheaterProvider.tsx
+++ src/context/TheaterProvider.tsx
@@ -1,34 +1,44 @@
-import { useState } from "react"
-import { theaters } from "../data/theaters"
-import TheaterContext from "./FilterContext"
-import { TheaterHourInfo } from "../types/theater"
-export const TheaterProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+import { useState } from "react";
+import { theaters } from "../data/theaters";
+import TheaterContext from "./FilterContext";
+import { Theater, TheaterHourInfo } from "../types/theater";
+export const TheaterProvider: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  const [theaterHourInfo, setTheaterHourInfo] = useState<TheaterHourInfo[]>([]);
+  const [isChecked, setIsChecked] = useState<boolean>(false);
+  const [theatersArray, setTheatersArray] = useState<Theater[]>(theaters);
 
-    const [theaterHourInfo, setTheaterHourInfo] = useState<TheaterHourInfo[]>([]);
-    const [isChecked, setIsChecked] = useState<boolean>(false);
+  const idMovieFunction = (slugId: string) => {
+    return parseInt(slugId?.split("-")[slugId.split("-").length - 1] || "0");
+  };
+  const theaterInfo = (id: number) => {
+    return theatersArray
+      .filter((theater) =>
+        theater.showTimes.some((showTime) => showTime.movieId === id)
+      )
+      .map((theater) => ({
+        ...theater,
+        showTimes: theater.showTimes.filter(
+          (showTime) => showTime.movieId === id
+        ),
+      }));
+  };
 
-    const idMovieFunction = (slugId: string) => {
-        return parseInt(slugId?.split("-")[slugId.split("-").length - 1] || "0");
-    }
-    const theaterInfo = (id: number) => {
-        return theaters
-            .filter(theater => theater.showTimes.some(showTime => showTime.movieId === id))
-            .map(theater => ({
-                ...theater,
-                showTimes: theater.showTimes.filter(showTime => showTime.movieId === id)
-            }))
-    }
-
-    return (
-        <TheaterContext.Provider value={{
-            theaterInfo,
-            idMovieFunction,
-            setTheaterHourInfo,
-            theaterHourInfo,
-            isChecked,
-            setIsChecked
-        }}>
-            {children}
-        </TheaterContext.Provider>
-    )
-}
+  return (
+    <TheaterContext.Provider
+      value={{
+        theaterInfo,
+        idMovieFunction,
+        setTheaterHourInfo,
+        theaterHourInfo,
+        isChecked,
+        setIsChecked,
+        theatersArray,
+        setTheatersArray,
+      }}
+    >
+      {children}
+    </TheaterContext.Provider>
+  );
+};
