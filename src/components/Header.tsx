10df6f7c4be338f20fead01d61ebaf75cd236020
DexterDC <sebasdc9@hotmail.com>
2025-01-09 10:45:05 -0600

    update name title

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 function Header() {
     return (
         <section className="h-80 bg-[#1E252B] flex items-center justify-center">
-            <h1 className="text-5xl text-white font-bold">rooms
+            <h1 className="text-5xl text-white font-bold">Cine Seats
                 <span className="text-red-500">.</span>
             </h1>
         </section>
